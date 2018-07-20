54dde80b5ad1b876604b26a58e4baccdd1450aa6
Ryan Guan <guan.ryan@gmail.com>
2018-07-19 23:16:14 -0400

    reorganized code with methods

--- TwitterScrape.py
+++ TwitterScrape.py
@@ -7,14 +7,24 @@ access_token = "ADD ACCESS TOKEN"
 access_token_secret = "ADD ACCESS TOKEN SECRET"
 
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
 
-api = tweepy.API(auth)
-print_count = 0;
-public_tweets = api.home_timeline(count = 100)
-for tweet in public_tweets:
-    print(tweet)
-    print_count += 1
 
-print (print_count)
+def simpleHomeTimelineScrape():
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+
+    api = tweepy.API(auth)
+    print_count = 0;
+    public_tweets = api.home_timeline(count=100)
+    for tweet in public_tweets:
+        print(tweet)
+        print_count += 1
+
+    print(print_count)
+
+
+def main():
+    simpleHomeTimelineScrape()
+
+if __name__ == '__main__':
+    main()
